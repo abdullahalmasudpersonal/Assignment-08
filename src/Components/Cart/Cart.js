50af6d6f9bb5db167bd54aa86557252b7065a1ef
masud <abdullahalmasud01737906772@gmail.com>
2022-04-03 14:15:17 +0600

    test

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -5,12 +5,12 @@ import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 const Cart = () => {
     return (
-        <div className='cart'>
-            <h3>Selected your product</h3>
-            <div className='div-cart-btn'>
-            <p> <button className='cart-btn'> CHOOSE 1 FOR ME</button></p>
+        <div className='cart '>
+            <h3 className='fw-bold'>Selected your product</h3>
+            <div>
+            <p> <button className='cart-btn fw-bold'> CHOOSE 1 FOR ME</button></p>
             <p>
-            <button className='cart-btn'> CHOOSE AGAIN</button>
+            <button className='cart-btn fw-bold'> CHOOSE AGAIN</button>
             </p>
             </div>
 
